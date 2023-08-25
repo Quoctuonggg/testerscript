dc08f5669c71d4386c391744e03001c3d96b2f20
qt??? <135344859+Quoctuonggg@users.noreply.github.com>
2023-08-25 13:51:46 +0700

    Add files via upload

--- 1.lua
+++ 1.lua
@@ -220,7 +220,7 @@ function loadSettings()
 end
 
 loadSettings()
-getgenv().Key = "1"
+getgenv().Key = "qtuongcuto"
 
 if _G.Key1 == getgenv().Key then
      TextBox.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -252,6 +252,9 @@ else
 	wait(0.5)
 	TextBox.TextColor3 = Color3.fromRGB(255, 0, 0)
 	TextBox.Text = "Key Wrong!"
+	wait(1)
+	TextBox.TextColor3 = Color3.fromRGB(255, 255, 255)
+	TextBox.Text = "Paste Your Key Here..."
 end
     TextButton1.MouseButton1Click:Connect(function()
 	local KeyInput = TextBox.Text
@@ -288,5 +291,8 @@ end
 		wait(0.5)
 		TextBox.TextColor3 = Color3.fromRGB(255, 0, 0)
 		TextBox.Text = "Key Wrong!"
+		wait(1)
+		TextBox.TextColor3 = Color3.fromRGB(255, 255, 255)
+		TextBox.Text = "Paste Your Key Here..."
 	end
 end)
